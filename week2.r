ab7d7472c3a6738fc4f4ef63c5c44cff3f0e11e5
Johnny Wharton <johnnyjw@gmail.com>
2020-11-20 08:19:12 +0000

    predicting with regression

--- week2.r
+++ week2.r
@@ -319,3 +319,55 @@ confusionMatrix(testing$type, predict(modelfit,testPC))
 modelFit <- train(type ~ ., method="glm",
                     preProcess = "pca", data = training)
 confusionMatrix(testing$type, predict(modelFit,testing))
+
+# predicting with regression
+# Old Faithful eruptions
+library(caret); data(faithful); set.seed(333)
+inTrain <- createDataPartition(y = faithful$waiting,
+                               p = 0.5,
+                               list = FALSE)
+trainFaith <- faithful[inTrain,]
+testFaith <- faithful[-inTrain,]
+head(trainFaith)
+plot(trainFaith$waiting, trainFaith$eruptions, pch=19, 
+     col="blue", xlab="Waiting", ylab="Duration")
+
+#train model linear
+lm1 <- lm(eruptions~waiting, data=trainFaith)
+summary(lm1)
+
+plot(trainFaith$waiting, trainFaith$eruptions, pch=19, 
+     col="blue", xlab="Waiting", ylab="Duration")
+lines(trainFaith$waiting, lm1$fitted, lwd=3)
+
+#the coefficients waiting=80
+coef(lm1)[1] + coef(lm1)[2]*80
+
+newdata <- data.frame(waiting=80)
+predict(lm1, newdata)
+
+# compare training and test
+par(mfrow=c(1, 2))
+plot(trainFaith$waiting, trainFaith$eruptions, pch=19, 
+     col="blue", xlab="Waiting", ylab="Duration")
+lines(trainFaith$waiting, predict(lm1), lwd=3)
+plot(testFaith$waiting, testFaith$eruptions, pch=19, 
+     col="blue", xlab="Waiting", ylab="Duration")
+lines(testFaith$waiting, predict(lm1, newdata = testFaith), lwd=3)
+
+#rmse on train
+sqrt(sum((lm1$fitted - trainFaith$eruptions)^2))
+#rmse on test
+sqrt(sum((predict(lm1, newdata = testFaith) - testFaith$eruptions)^2))
+
+# prediction intervals
+pred1 <- predict(lm1, newdata = testFaith, interval="prediction")
+ord <- order(testFaith$waiting)
+par(mfrow=c(1, 1))
+plot(testFaith$waiting, testFaith$eruptions, pch=19, 
+     col="blue", xlab="Waiting", ylab="Duration")
+matlines(testFaith$waiting[ord], pred1[ord,], type='l', ,col=c(1,2,2),lty=c(1,1,1), lwd=3)
+
+#using carat
+modFit <- train(eruptions~waiting, data=trainFaith, method="lm")
+summary(modFit$finalModel)
